daba1f83431c0623a8a0d3dd94c76c3694aa97ea
gilaku <gilarbos@yahoo.es>
2021-07-26 10:38:46 +0200

    fem el commit dels canvis després de fer el clone

--- main.c
+++ main.c
@@ -1,1 +1,6 @@
 fem e primer fitxer de c pel git
+
+ja he esborrat el local i després fet un clone del projecte per tornar-lo a tenir al disc. 
+ara tornaré a fer un commit
+
+ara que ja he fet el commit(stage) miraré de comparar-lo amb aquesta versió nova
