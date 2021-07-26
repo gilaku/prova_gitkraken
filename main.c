79adb3327c61f828acd20d5202716eb4b379fc2f
gilaku <gilarbos@yahoo.es>
2021-07-26 10:42:25 +0200

    aquest es amb els comentaris abans de fer el push

--- main.c
+++ main.c
@@ -4,3 +4,5 @@ ja he esborrat el local i després fet un clone del projecte per tornar-lo a ten
 ara tornaré a fer un commit
 
 ara que ja he fet el commit(stage) miraré de comparar-lo amb aquesta versió nova
+
+primer era fer el stage, que es com marcar-lo sper despres fes el commit, ara cal fer el push
